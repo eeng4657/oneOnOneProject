e7c86f5aaed525c292b8a1b77509bdd2612d0e58
eeng4657 <eeng4657@bths.edu>
2023-11-06 00:35:01 -0500

    completed fighter stats function

--- js/fighter.js
+++ js/fighter.js
@@ -3,12 +3,27 @@ class Fighter{
     counter=0;
 
     func(){
-        while (counter < 2){
-            let int=this.stats[Math.floor(Math.random(4))];
-            if (Number.isInteger(int))
+        while(counter<2){
+            let int=this.stats[Math.floor(Math.random()*4)];
+            if(Number.isInteger(int))
             {
                 stats[int]=stats[int].toString();
                 counter++;
+            }
+        }
+        counter=0;
+        while(counter<4){
+            if(stats[counter].isInteger){
+                if(counter===3){
+                    stats[counter]=30+Math.floor(Math.random()*7);
+                }
+                else{stats[counter]=6+Math.floor(Math.random()*2);}
+            }
+            else{
+                if(counter===3){
+                    stats[counter]=30-Math.floor(Math.random()*7);
+                }
+                else{stats[counter]=6-Math.floor(Math.random()*2);}
             }
         }
     }
