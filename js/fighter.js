f3d87de59137618054b765f4f09ee8a6a0f0a812
eeng4657 <eeng4657@bths.edu>
2023-11-05 23:50:05 -0500

    added tagging to fighter stats

--- js/fighter.js
+++ js/fighter.js
@@ -1,4 +1,15 @@
 class Fighter{
-    stats=[];
-    
+    stats=(1,2,3,4);
+    counter=0;
+
+    func(){
+        while (counter < 2){
+            let int=this.stats[Math.floor(Math.random(4))];
+            if (Number.isInteger(int))
+            {
+                stats[int]=stats[int].toString();
+                counter++;
+            }
+        }
+    }
 }
