de10539190b450f34d749670c88b10f016f12571
eeng4657 <eeng4657@bths.edu>
2023-11-10 09:33:22 -0500

    edited attack and defend checking

--- js/fighter.js
+++ js/fighter.js
@@ -98,48 +98,6 @@ class Fighter{ //creates a Fighter instance
     }
 }
 
-//allows both players to choose what action they want to take, then executes them after both players have chosen
-const round=(f1,f2)=>{
-    if(!((f1.getStat(3)>=(f2.getStat(3)*2)) && f2.getStat(3))<=0){
-        if((Math.floor(Math.random()*2))=0){
-            f2.setAttack(true); f2.setDefend(false); f2.setFinish(false);
-        }
-        else{f2.setAttack(false); f2.setDefend(true);}
-    }
-    else{f2.setAttack(true); f2.setDefend(false); f2.setFinish(true);}
-
-    attack.addEventListener('click',f1.setAttack(true) && f1.setDefend(false) && f1.setFinish(false));
-    defend.addEventListener('click',f1.setDefend(true) && f1.setAttack(false));
-    finisher.addEventListener('click',()=>{
-        if((f2.getStat(3)>=(f1.getStat(3)*2)) && f1.getStat(3)<=0){
-            f1.setAttack(true);
-            f1.setDefend(false);
-            f1.setFinish(true);
-        }
-    });
-
-    if(f1.getFinish() && f1.getAttack()>1 && !isOver){
-        message.innerHTML='You win.';
-        f1Log.innerHTML+='finish\n';
-        isOver=true;
-    }
-    if(f2.getFinish() && f2.getAttack()>1 && !isOver){
-        message.innerHTML='You lose.';
-        f2Log.innerHTML+='finish\n';
-        isOver=true;
-    }
-    if((f1.getAttack()===0)&&(f2.getAttack()===0)){
-        f1.setStat(3, Math.floor((Math.random()*6)+1)); fighter1.updateStatDisplay(f1Stats); f1Log.innerHTML+='defend\n';
-        f2.setStat(3, Math.floor((Math.random()*6)+1)); fighter2.updateStatDisplay(f2Stats); f2Log.innerHTML+='defend\n';
-    }
-    if(f1.getAttack()!==0 && f1.getAttack()>f2.getDefend()){
-        f2.setStat(3,f2.getDefend()-f1.getAttack()); fighter2.updateStatDisplay(f2Stats);
-    }
-    if(f2.getAttack()!==0 && f2.getAttack()>f1.getDefend()){
-        f1.setStat(3,f1.getDefend()-f2.getAttack()); fighter1.updateStatDisplay(f1Stats);
-    }
-}
-
 const game=()=>{
     let fighter1=new Fighter();
     fighter1.randomize();
@@ -151,5 +109,50 @@ const game=()=>{
     fighter2.fillOGStats(f2OG);
     fighter2.updateStatDisplay(f2Stats);
 
+    //allows both players to choose what action they want to take, then executes them after both players have chosen
+    const round=(f1,f2)=>{
+        if(!((f1.getStat(3)>=(f2.getStat(3)*2)) && f2.getStat(3))<=0){
+            if((Math.floor(Math.random()*2))=0){
+                f2.setAttack(true); f2.setDefend(false); f2.setFinish(false);
+            }
+            else{f2.setAttack(false); f2.setDefend(true);}
+        }
+        else{f2.setAttack(true); f2.setDefend(false); f2.setFinish(true);}
+
+        attack.addEventListener('click',f1.setAttack(true) && f1.setDefend(false) && f1.setFinish(false));
+        defend.addEventListener('click',f1.setDefend(true) && f1.setAttack(false));
+        finisher.addEventListener('click',()=>{
+            if((f2.getStat(3)>=(f1.getStat(3)*2)) && f1.getStat(3)<=0){
+                f1.setAttack(true);
+                f1.setDefend(false);
+                f1.setFinish(true);
+            }
+        });
+
+        if(f1.getFinish() && f1.getAttack()>1 && !isOver){
+            message.innerHTML='You win.';
+            f1Log.innerHTML+='finish\n';
+            isOver=true;
+        }
+        if(f2.getFinish() && f2.getAttack()>1 && !isOver){
+            message.innerHTML='You lose.';
+            f2Log.innerHTML+='finish\n';
+            isOver=true;
+        }
+        if((f1.getAttack()===0)&&(f2.getAttack()===0)){
+            f1.setStat(3, Math.floor((Math.random()*6)+1)); fighter1.updateStatDisplay(f1Stats); f1Log.innerHTML+='defend\n';
+            f2.setStat(3, Math.floor((Math.random()*6)+1)); fighter2.updateStatDisplay(f2Stats); f2Log.innerHTML+='defend\n';
+        }
+        if(f1.getAttack()!==0){
+            f1Log.innerHTML+='attack\n';
+            if(f1.getAttack()>f2.getDefend()){f2.setStat(3,f1.getAttack()-f2.getDefend()); fighter2.updateStatDisplay(f2Stats);}
+            if(f2.getAttack===0){f2Log.innerHTML+='defend\n';}
+        }
+        if(f2.getAttack()!==0){
+            f2Log.innerHTML+='attack\n';
+            if(f2.getAttack()>f1.getDefend()){f1.setStat(3,f2.getAttack()-f1.getDefend()); fighter1.updateStatDisplay(f1Stats);}
+            if(f1.getAttack===0){f1Log.innerHTML+='defend\n';}
+        }
+    }
     while(!isOver){round(fighter1,fighter2);}
 }
